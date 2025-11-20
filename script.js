6eb9463aa81264b6c072ce66da6429b1da512478
glushkopasha053-web <glushkopasha053@gmail.com>
2025-11-20 13:09:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Знаходимо кнопку по ID
+const button = document.getElementById("mainBtn");
+
+// Вішаємо обробник події "click"
+button.addEventListener("click", function() {
+
+    // Коли кнопка натиснута — показуємо повідомлення
+    alert("Дякуємо! Ми скоро з вами зв’яжемося ❤️");
+});
